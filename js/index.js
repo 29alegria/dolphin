d9129920b0402559cf5fa83c453748b0cfe9ed14
29alegria <29alegria.leso@gmail.com>
2024-12-13 11:03:24 +0900

    openingアニメーション

--- js/index.js
+++ js/index.js
@@ -1,13 +1,37 @@
-gsap.timeline() 
-  .from(".js_copy", {
-    duration: 1.5,
-    opacity: 0,
-    x: -100,
-    ease: "power3.out",
-  })
-  .from(".js_sub-copy", {
-    duration: 1.5,
-    opacity: 0,
-    x: -100,
-    ease: "power3.out",
-  })
+function openingAnime() {
+  const body = document.querySelector(".js_body");
+  body.classList.toggle("is-active");
+
+  gsap.timeline()
+    .from(".js_opening-logo",{
+      duration: 1.2,
+      autoAlpha: 0,
+      y: 40,      
+    }).from(".js_opening-txt", {
+      duration: 2,
+      autoAlpha: 0,
+      y: 40,
+    }).to(".js_opening", {
+      autoAlpha: 0,
+      duration: 2,
+      onComplete: function () {
+        body.classList.toggle("is-active");
+      },
+    })
+    .from(".js_copy", {
+      duration: 0.6,
+      autoAlpha: 0,
+      x: -100,
+      ease: "power3.out",
+    })
+    .from(".js_sub-copy", {
+      duration: 0.6,
+      autoAlpha: 0,
+      x: -100,
+      ease: "power3.out",
+    });
+};
+
+openingAnime();
+
+

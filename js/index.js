208b46e68e5148c81903ca5c1b371f556d1239df
29alegria <29alegria.leso@gmail.com>
2024-12-13 11:34:09 +0900

    top_kv_copyのアニメーション追加

--- js/index.js
+++ js/index.js
@@ -29,7 +29,19 @@ function openingAnime() {
       autoAlpha: 0,
       x: -100,
       ease: "power3.out",
-    });
+    })
+  // 1行目の下線
+    .to(".js_copy .line1", {
+      duration: 1,
+      backgroundSize: "100% 8px",
+      ease: "power1.out"
+    })
+    // 前のアニメーション完了後、少し遅らせて2行目の下線
+    .to(".js_copy .line2", {
+      duration: 1,
+      backgroundSize: "100% 8px",
+      ease: "power1.out"
+    }, "+=0.2"); // 0.2秒遅延など好みで調整
 };
 
 openingAnime();
